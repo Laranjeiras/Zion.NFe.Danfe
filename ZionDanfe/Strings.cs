4dab8da248c3c83ca2b7db38cf29bdf756043837
Alexandre Laranjeiras <laranja22@hotmail.com>
2021-01-03 15:22:45 -0300

    Modificado créditos

--- ZionDanfe/Strings.cs
+++ ZionDanfe/Strings.cs
@@ -12,8 +12,8 @@
         public const string NomeRazaoSocial = "Nome / Razão Social";
         public const string FoneFax = "Fone / Fax";
         public const string Cep = "CEP";
-        public const string BairroDistrito = "Bairro / Distrito";
+        public const string BairroDistrito = "Bairro";
         public const string TextoConsulta = "Consulta de autenticidade no portal nacional da NF-e www.nfe.fazenda.gov.br/portal ou no site da Sefaz Autorizadora";
-        public static string TextoCreditos = "Desenvolvido por www.laranjeiras.dev (zideun@laranjeiras.dev)";
+        public static string TextoCreditos = "Desenvolvido por [ www.laranjeiras.dev / (21)997706037 ]";
     }
 }
