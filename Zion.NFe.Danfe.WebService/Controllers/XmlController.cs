3c70e8ab2a3a42e7c39c8d02d8bdca908303f23b
Alexandre Laranjeiras <laranja22@hotmail.com>
2021-01-03 16:34:24 -0300

    Criado uima Api para gerar string xml em PDF.

---
+++ Zion.NFe.Danfe.WebService/Controllers/XmlController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Zion.NFe.Danfe;
+using Zion.NFe.Danfe.Modelo;
+
+namespace ZionDanfe.WebService.Controllers
+{
+    public class XmlController : Controller
+    {
+        [HttpPost("/api/xml/pdf/gerar")]
+        public async Task<IActionResult> Index()
+        {
+            string xml = null;
+
+            using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                xml = await reader.ReadToEndAsync();
+            }
+
+            var modelo = DanfeViewModelCreator.CriarDeStringXml(xml);
+
+            using (var pdfStream = new MemoryStream())
+            {
+                using (var danfe = new DanfeDoc(modelo))
+                {
+                    danfe.ViewModel.DefinirTextoCreditos("Desenvolvido por [ www.laranjeiras.dev / (21)997706037 ]");
+                    danfe.Gerar();
+                    var bytesPdf = danfe.ObterPdfBytes(pdfStream);
+                    return File(bytesPdf, "Application/pdf", $"{modelo.ChaveAcesso}.pdf");
+                }
+            }
+        }
+    }
+}
